56824febc7cc710f529820a80a839140a0094852
rinkuberde <rinkuberde@gmail.com>
2023-01-27 18:34:07 +0530

    [Rinku] - Coffee Maker Demo

---
+++ CoffeeMakerDemo/IMakeACoffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMakerDemo
+{
+    public interface IMakeACoffee
+    {
+        bool CheckIngredientAvailability();
+        string CoffeeMaking(int sugarPerSpoon, int coffeeCount);
+
+        /*public interface IMakeACoffee
+        {
+            bool CheckIngredientAvailability();
+            string CoffeeMaking(int sugerPerSpoon, int CoffeePack);
+        }*/
+    }
+}
